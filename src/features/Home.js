e548810f33ffdbcab6e087d848bc77451d87b643
Rachanon (RCN) <143895811+SaiMaiNow@users.noreply.github.com>
2025-10-12 11:35:59 +0700

    lab 8

--- src/features/Home.js
+++ src/features/Home.js
@@ -1,18 +1,28 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
-import data from '../app/data';
+// import data from '../app/data';
 import Product from './Product';
 import AddForm from './Product/AddForm';
 
 let currentProductId = 9;
 
 export default function Home() {
-  const [products, setProducts] = useState(data);
+  const [products, setProducts] = useState([]);
 
   function addProduct(product) {
     const newProduct = { id: ++currentProductId, ...product };
     setProducts([...products, newProduct]);
   }
+
+  async function getProducts() {
+    const products = await axios.get(
+      'https://apimocha.com/react-redux-class/products'
+    );
+    setProducts(products.data);
+  }
+
+  getProducts();
 
   return (
     <>
